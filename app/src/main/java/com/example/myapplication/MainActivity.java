30d8bcfcced145259905394c512ef3744d079ca6
Rawad-Yakub <yakubrawad@gmail.com>
2022-03-17 22:37:44 -0400

    New files

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,27 +1,40 @@
 package com.example.myapplication;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView flashcardQuestion;
+    TextView flashcardAnswer;
+    TextView flashcardAnswer2;
+    TextView flashcardAnswer3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
-        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
-        TextView flashcardAnswer2 = findViewById(R.id.flashcard_answer2);
-        TextView flashcardAnswer3 = findViewById(R.id.flashcard_answer3);
+
+        flashcardQuestion = findViewById(R.id.flashcard_question);
+        flashcardAnswer = findViewById(R.id.flashcard_answer);
+        flashcardAnswer2 = findViewById(R.id.flashcard_answer2);
+        flashcardAnswer3 = findViewById(R.id.flashcard_answer3);
 
         findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
         findViewById(R.id.flashcard_answer2).setVisibility(View.INVISIBLE);
         findViewById(R.id.flashcard_answer3).setVisibility(View.INVISIBLE);
+
+        View addbutton = findViewById(R.id.addcard);
 
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer3).setVisibility(View.VISIBLE);
+
 
             }
         });
@@ -56,5 +70,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoAddCard = new Intent(MainActivity.this, AddCardActivity.class);
+ //                   startActivity(gotoAddCard);
+                startActivityForResult(gotoAddCard, 100);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100){
+            if (data != null){
+                String questionString = data.getExtras().getString("Question key");
+                String answerString = data.getExtras().getString("Answer key");
+                String wrong1String = data.getExtras().getString("Wrong Answer1");
+                String wrong2String = data.getExtras().getString("Wrong Answer2");
+                flashcardQuestion.setText(questionString);
+                flashcardAnswer.setText(answerString);
+                flashcardAnswer2.setText(wrong1String);
+                flashcardAnswer3.setText(wrong2String);
+                Snackbar.make(findViewById(R.id.flashcard_question),
+                        "Successfully added question",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+
+            }
+        }
     }
 }
